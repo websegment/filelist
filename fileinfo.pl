b49cae6394ecf97150d714d3d63cebd8ca4d6ebc
Daisuke Sakata <sakatad@gmail.com>
2012-06-13 23:48:29 +0900

    ファイル名に記号が入っていた時の対応
    日本語コメントを消した。
    各正規表現入れてみた。

--- fileinfo.pl
+++ fileinfo.pl
@@ -5,21 +5,45 @@ use strict;
 my @chown;
 my @chmod;
  
-#ディレクトリの指定
+# Directory
 my $directory = $ARGV[0];
 my @filelist = `find $directory -ls`;
 foreach my $file(@filelist){
 	$file =~s/\s+/\t/g;
 	my @info = split(/\t/, $file);
-	#ファイルのステータスを取得
+	my $user = $info[4];
+	my $group = $info[5];
+	my $filepath = $info[10];
+	$filepath =~ s/\'/\\'/ig;
+	$filepath =~ s/\"/\\"/ig;
+	$filepath =~ s/\(/\\(/ig;
+	$filepath =~ s/\)/\\)/ig;
+	$filepath =~ s/\>/\\>/ig;
+	$filepath =~ s/\</\\</ig;
+	$filepath =~ s/\{/\\{/ig;
+	$filepath =~ s/\}/\\}/ig;
+	$filepath =~ s/\[/\\[/ig;
+	$filepath =~ s/\]/\\]/ig;
+	$filepath =~ s/\|/\\|/ig;
+	$filepath =~ s/\=/\\=/ig;
+	$filepath =~ s/\`/\\`/ig;
+	$filepath =~ s/\@/\\@/ig;
+	$filepath =~ s/\:/\\:/ig;
+	$filepath =~ s/\*/\\*/ig;
+	$filepath =~ s/\?/\\?/ig;
+	$filepath =~ s/$/\$/ig;
+	$filepath =~ s/\&/\\&/ig;
+	$filepath =~ s/\!/\\!/ig;
+
+	# Get file status.
 	my @status = stat $info[10];
-	#パーミッションを8進数に変換
+	# Convert permission
 	my $permission = substr((sprintf "%03o", $status[2]), -3);
 
 	my $chownCommand = sprintf("chown %s:%s %s\n",
-		$info[4], $info[5], $info[10]);
+		$user, $group, $filepath);
 	my $chmodCommand = sprintf("chmod %s %s\n",
-		$permission, $info[10]);
+		$permission, $filepath);
 	push(@chown, $chownCommand);
 	push(@chmod, $chmodCommand);
 }
